99bbd1e0ef8648cc53b7bc650bb451a5a0eb58e2
silvamq <silvamq@gmail.com>
2019-03-22 00:54:01 -0300

    up

---
+++ docker/spark-standalone/image/sh/master.sh
@@ -0,0 +1,2 @@
+/spark-2.4.0-bin-hadoop2.7/sbin/start-master.sh
+tail -f /spark-2.4.0-bin-hadoop2.7/logs/*
---
+++ docker/spark-standalone/image/sh/slave.sh
@@ -0,0 +1,2 @@
+/spark-2.4.0-bin-hadoop2.7/sbin/start-slave.sh spark://master:7077
+tail -f /spark-2.4.0-bin-hadoop2.7/logs/*
